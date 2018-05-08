a39216cf59c2595a9355e33ebadb5975b5a2ff22
pandukaloc <panduka29@gmail.com>
2018-05-08 09:01:24 +0530

    monday changes

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -4,119 +4,119 @@ jQuery(document).ready(function($) {
 
     jQuery("#time_countdown1").countDown({
 
-     targetDate: {
-       'day':    10,
-       'month':  10,
-       'year':   2017,
-       'hour':   11,
-       'min':    0,
-       'sec':    0
-   },
-   omitWeeks: true
-});
+        targetDate: {
+            'day':    10,
+            'month':  10,
+            'year':   2017,
+            'hour':   11,
+            'min':    0,
+            'sec':    0
+        },
+        omitWeeks: true
+    });
 
     jQuery("#time_countdown2").countDown({
 
-     targetDate: {
-       'day': 1,
-       'month': 12,
-       'year': 2016,
-       'hour': 0,
-       'min': 0,
-       'sec': 0
-   },
-   omitWeeks: true
-});
+        targetDate: {
+            'day': 1,
+            'month': 12,
+            'year': 2016,
+            'hour': 0,
+            'min': 0,
+            'sec': 0
+        },
+        omitWeeks: true
+    });
 
     jQuery("#time_countdown3").countDown({
 
-     targetDate: {
-       'day': 1,
-       'month': 12,
-       'year': 2020,
-       'hour': 0,
-       'min': 0,
-       'sec': 0
-   },
-    //   omitWeeks: true
-});
+        targetDate: {
+            'day': 1,
+            'month': 12,
+            'year': 2020,
+            'hour': 0,
+            'min': 0,
+            'sec': 0
+        },
+        //   omitWeeks: true
+    });
 
     jQuery("#time_countdown4").countDown({
 
-     targetDate: {
-       'day': 1,
-       'month': 12,
-       'year': 2020,
-       'hour': 0,
-       'min': 0,
-       'sec': 0
-   },
-    //   omitWeeks: true
-});
+        targetDate: {
+            'day': 1,
+            'month': 12,
+            'year': 2020,
+            'hour': 0,
+            'min': 0,
+            'sec': 0
+        },
+        //   omitWeeks: true
+    });
 
     jQuery("#time_countdown5").countDown({
 
-     targetDate: {
-       'day': 1,
-       'month': 12,
-       'year': 2020,
-       'hour': 0,
-       'min': 0,
-       'sec': 0
-   },
-   //    omitWeeks: true
-});
+        targetDate: {
+            'day': 1,
+            'month': 12,
+            'year': 2020,
+            'hour': 0,
+            'min': 0,
+            'sec': 0
+        },
+        //    omitWeeks: true
+    });
 
-   //    $("#time_countdown-6").countDown({
+    //    $("#time_countdown-6").countDown({
 
-   //       targetDate: {
-   //         'day': 1,
-   //         'month': 12,
-   //         'year': 2020,
-   //         'hour': 0,
-   //         'min': 0,
-   //         'sec': 0
-   //     },
-   // //    omitWeeks: true
-   //    });
+    //       targetDate: {
+    //         'day': 1,
+    //         'month': 12,
+    //         'year': 2020,
+    //         'hour': 0,
+    //         'min': 0,
+    //         'sec': 0
+    //     },
+    // //    omitWeeks: true
+    //    });
 
-jQuery("#time_countdown7").countDown({
+    jQuery("#time_countdown7").countDown({
 
- targetDate: {
-   'day': 1,
-   'month': 12,
-   'year': 2020,
-   'hour': 0,
-   'min': 0,
-   'sec': 0
-},
-   //    omitWeeks: true
-});
+        targetDate: {
+            'day': 1,
+            'month': 12,
+            'year': 2020,
+            'hour': 0,
+            'min': 0,
+            'sec': 0
+        },
+        //    omitWeeks: true
+    });
 
-jQuery("#time_countdown8").countDown({
+    jQuery("#time_countdown8").countDown({
 
- targetDate: {
-   'day': 1,
-   'month': 12,
-   'year': 2020,
-   'hour': 0,
-   'min': 0,
-   'sec': 0
-},
-   //    omitWeeks: true
-});
+        targetDate: {
+            'day': 1,
+            'month': 12,
+            'year': 2020,
+            'hour': 0,
+            'min': 0,
+            'sec': 0
+        },
+        //    omitWeeks: true
+    });
 
-jQuery("#time_countdown9").countDown({
+    jQuery("#time_countdown9").countDown({
 
- targetDate: {
-   'day': 1,
-   'month': 12,
-   'year': 2020,
-   'hour': 0,
-   'min': 0,
-   'sec': 0
-},
-  //     omitWeeks: true
-});
+        targetDate: {
+            'day': 1,
+            'month': 12,
+            'year': 2020,
+            'hour': 0,
+            'min': 0,
+            'sec': 0
+        },
+        //     omitWeeks: true
+    });
 
 });
